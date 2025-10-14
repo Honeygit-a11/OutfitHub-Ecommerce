563ae274c8227f07aec870ed202f5208b8e1cb57
Honey Kumar <hk9484678@gmail.com>
2025-10-14 23:12:30 +0530

    Add build script

--- frontend/src/components/Productzoom.jsx
+++ frontend/src/components/Productzoom.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import '../Style/ProductZoom.css'
+import '../Style/Productzoom.css'
 const ProductZoom = ({src,alt}) =>{
     const [backgroundPos,setBackgroundPos] = useState('center');
 
